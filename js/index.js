33dd9b6093f1c3b91ac67f67386fa413af414370
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2023-11-22 17:21:59 +0100

    Progress commit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,28 @@
 const currentWeather = document.getElementById('current');
 const date = document.getElementById('current-date');
 const time = document.getElementById('current-time');
-const month = new Date().getMonth();
 
-date.textContent = 'Idag' + ' ' + new Date().getDate() + ' ' + currentMonth(month);
-time.textContent = new Date().getHours() + ':' + new Date().getMinutes();
+getCurrentDate();
+getCurrentTime();
 
-function currentMonth(month) {
+function getCurrentDate() {
 
+    const now = new Date();
+    const month = now.getMonth();
     const months = [
         'januari', 'februari', 'mars', 'april', 'maj', 'juni',
         'juli', 'augusti', 'september', 'oktober', 'november', 'december'
       ];
 
-    return months[month];
-
+    date.textContent = 'Idag' + ' ' + now.getDate() + ' ' + months[month];
 }
+
+function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const correctMinutes = (minutes < 10) ? '0' + minutes : minutes;
+    time.textContent = hours + ':' + correctMinutes;
+}
+
+setInterval(getCurrentTime, 1000);
